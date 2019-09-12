d284707b42434aebad5b5595767320253022a83b
Nick Schaub <nick.schaub@nih.gov>
2019-09-12 14:57:37 -0400

    Functional plugin.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import argparse
 import time
 from pathlib import Path
 from bfio import czi2tif
+import javabridge as jutil
 
 def main():
     # Setup the Argument parsing
@@ -22,6 +23,12 @@ def main():
     print('output_dir = {}'.format(output_dir))
 
     print(' ')
+    
+    print('Initializing the javabridge...')
+    JARS = jutil.JARS
+    bfjar = jutil.JARS[0][0:jutil.JARS[0].find("javabridge")]
+    JARS.append(bfjar + "bioformats/jars/loci_tools.jar")
+    jutil.start_vm(class_path=JARS)
 
     print('Extracting tiffs and saving as ome.tif...')
     files = [f for f in Path(input_dir).iterdir() if f.is_file() and f.suffix=='.czi']
@@ -30,11 +37,12 @@ def main():
     
     for file in files:
         start_time = time.time()
-        print('Starting extraction from ' + file.absolute() + '...')
-        czi2tif.write_ome_tiffs(file.absolute(),output_dir)
+        print('Starting extraction from ' + str(file.absolute()) + '...')
+        czi2tif.write_ome_tiffs(str(file.absolute()),output_dir)
         print('Finished in {}s!'.format(time.time()-start_time))
         
-    print('Finished extracting files. Exiting...')
+    print('Finished extracting files. Closing the javabridge and exiting...')
+    jutil.kill_vm()
 
 if __name__ == "__main__":
     main()
